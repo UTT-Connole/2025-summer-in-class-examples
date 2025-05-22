8d8f746d7b7926756b0825f9fd97a462c6505289
Brayden Connole <brayden.connole@gmail.com>
2025-05-22 17:33:44 -0600

    some examples in Nextjs

---
+++ next-js-example/src/app/stephen/page.tsx
@@ -0,0 +1,10 @@
+const Stephen = () => {
+  return (
+    <div>
+      <h1>Stephen</h1>
+      <p>Stephen is a software engineer.</p>
+    </div>
+  );
+}
+
+export default Stephen;
---
+++ next-js-example/src/app/topher/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+    return (
+        <div>
+        <h1>Topher</h1>
+        <p>This is the Topher page.</p>
+        </div>
+    );
+}
+
+export default Page;
